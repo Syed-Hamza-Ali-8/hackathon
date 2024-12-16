9eeb368985e5261dbf2e26008978d92f8c23bb10
Syed-Hamza-Ali-8 <ha5755420@gmail.com>
2024-12-17 00:02:31 +0500

    Hackathon

--- src/app/signup/Hero.tsx
+++ src/app/signup/Hero.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import Link from 'next/link'
 
@@ -8,7 +7,7 @@ function Hero() {
       <div className='container mx-auto px-4'>
         <div className='flex flex-col items-center'>
           <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight text-white font-bold text-center mb-4 sm:mb-6'>
-            Sign up page
+            Sign up 
           </h1>
           <div className='text-base sm:text-lg md:text-xl flex gap-2 text-center justify-center'>
             <Link href="/" className='text-white hover:text-[#FF9F0D] transition-colors duration-300'>
